30788096c5da5730d1334d8e90092b13e2a8aa0f
Joshua Elusoji <joshuaelusoji7@gmail.com>
2026-09-11 17:00:17 +0100

    features: project setup, nativewind, routing, tab navigation

---
+++ app/WelcomeScreen.tsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native'
+
+const WelcomeScreen = () => {
+    return (
+        <View>
+            <Text>WelcomeScreen</Text>
+        </View>
+    )
+}
+export default WelcomeScreen
---
+++ app/orders/[id].tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+import {Link, useLocalSearchParams} from "expo-router";
+
+const OrderDetails = () => {
+    const { id } = useLocalSearchParams<{ id: string }>();
+    return (
+        <View>
+            <Text>Order Details: {id}</Text>
+            <Link href="/">Go back</Link>
+        </View>
+    )
+}
+    export default OrderDetails
